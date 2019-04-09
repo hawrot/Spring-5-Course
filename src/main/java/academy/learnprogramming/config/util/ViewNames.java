c1e7911c751297e81a1bf25e475090efbaf9a57e
hawrot <mhawro200@caledonian.ac.uk>
2019-04-09 13:19:11 +0100

    Add ViewNames to util package

---
+++ src/main/java/academy/learnprogramming/config/util/ViewNames.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming.config.util;
+
+public final class ViewNames {
+
+    // == constants ==
+    public static final String ITEMS = "items";
+
+    // == constructors ==
+    private ViewNames(){}
+}
