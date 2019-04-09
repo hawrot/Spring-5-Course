fb0419b5ad5f0e404cb5afb18ade4c09189eab69
hawrot <mhawro200@caledonian.ac.uk>
2019-04-09 12:47:29 +0100

    Add model

--- src/main/java/academy/learnprogramming/config/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/config/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
@@ -35,10 +36,12 @@ public class DemoController {
     }
 
     // http://localhost:8080/todo-list/welcome
+    // http://localhost:8080/todo-list/welcome?user=Tim
     @GetMapping("welcome")
-    public String welcome(Model model){
+    public String welcome(@RequestParam String user, @RequestParam int age, Model model){
 
-        model.addAttribute("helloMessage", demoService.getHelloMessage("Tim"));
+        model.addAttribute("helloMessage", demoService.getHelloMessage(user));
+        model.addAttribute("age", age);
         log.info("model = {}", model);
         return "welcome";
     }
