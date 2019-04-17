c8829efd9ce143d3bdca68a90715d77fb45dce32
hawrot <mhawro200@caledonian.ac.uk>
2019-04-17 14:20:06 +0100

    Update ViewNames

---
+++ web/src/main/java/academy/learnprogramming/util/AtrributeNames.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming.util;
+
+public final class AtrributeNames {
+
+    // -- constants ==
+   public static final String MAIN_MESSAGE =  "mainMessage";
+   public static final String RESULT_MESSAGE =  "resultMessage";
+
+    // constructor --
+
+
+    public AtrributeNames() {
+    }
+}
