7c5c83056ee55fde057dd939f179e87e96387a6f
hawrot <mhawro200@caledonian.ac.uk>
2019-04-17 14:19:36 +0100

    Add GameMappings

---
+++ web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming.util;
+
+public class GameMappings {
+
+    // -- constants --
+
+    public static final String PLAY = "play";
+    public static final String REDIRECT_PLAY = "redirect:/" + PLAY;
+
+    // -- constructor --
+
+
+    public GameMappings() {
+    }
+}
