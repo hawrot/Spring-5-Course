b942a5e232ccc021ff43b574d4f3f3fbe42a5034
hawrot <mhawro200@caledonian.ac.uk>
2019-04-17 14:19:59 +0100

    Update ViewNames

---
+++ web/src/main/java/academy/learnprogramming/controller/GameController.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class GameController {
+
+    // --fields --
+}
